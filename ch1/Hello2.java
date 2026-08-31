1cfa2dbfcabb7ab8b5150e4b65a968118d133e4e
Jonathon Meyer <jon.projectloki@gmail.com>
2026-08-31 16:44:59 -0400

    Established VS Code settings to prevent package declarations, and wrote all ch1 programs.

---
+++ ch1/Hello2.java
@@ -0,0 +1,10 @@
+// FileName Hello2.java
+// Written by: Jonathon Meyer
+// Written on: 08/31/2026
+
+public class Hello2
+{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
---
+++ ch1/HelloDialog.java
@@ -0,0 +1,11 @@
+// FileName HelloDialog.java
+// Written by: Jonathon Meyer
+// Written on: 08/31/2026
+
+import javax.swing.JOptionPane;
+
+public class HelloDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+    }
+}
