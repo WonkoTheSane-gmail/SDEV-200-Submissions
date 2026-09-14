8fe2070a4f9d17fe89bf8931f5af9d3cfab74022
Jonathon Meyer <jon.projectloki@gmail.com>
2026-09-14 15:51:42 -0400

    Chapter 3 partial

---
+++ ch3/You Do It/DemoOverload.java
@@ -0,0 +1,25 @@
+// FileName DemoOverload.java
+// Written by: Jonathon Meyer
+// Written on: 09/14/2026
+
+public class DemoOverload {
+    public static void main(String[] args) {
+        int month = 6, day = 24, year = 2023;
+
+        displayDate(month);
+        displayDate(month, day);
+        displayDate(month, day, year);
+    }
+    
+    public static void displayDate(int mm) {
+        System.out.println("Event date: " + mm + "/1/2024");
+    }
+
+    public static void displayDate(int mm, int dd) {
+        System.out.println("Event date: " + mm + "/" + dd + "/2024");
+    }
+
+    public static void displayDate(int mm, int dd, int yyyy) {
+        System.out.println("Event date: " + mm + "/" + dd + "/" + yyyy);
+    }
+}
---
+++ ch3/You Do It/TestInfo.java
@@ -0,0 +1,10 @@
+// FileName TestInfo.java
+// Written by: Jonathon Meyer
+// Written on: 09/14/2026
+
+public class TestInfo {
+    public static void main(String[] args) {
+        System.out.println("Calling a method from another class:");
+        ParadiseInfo.displayInfo();
+    }
+}
